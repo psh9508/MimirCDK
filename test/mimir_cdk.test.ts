f7b996b3ea41440895979842d80b10e14f265e54
masuri <psh0258@gmail.com>
2025-12-22 10:14:23 +0900

    feat: initialize CDK project with TypeScript configuration, package.json, and example test

---
+++ test/mimir_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MimirCdk from '../lib/mimir_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mimir_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MimirCdk.MimirCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
